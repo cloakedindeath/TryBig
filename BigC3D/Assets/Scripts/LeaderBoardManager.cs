7a7e44ee15fb9ac9f2e2a65715dd85709868c24e
Lenovo-PC\Daniel <deathcloak@gmail.com>
2018-03-14 16:52:30 -0400

    hope its good

--- BigC3D/Assets/Scripts/LeaderBoardManager.cs
+++ BigC3D/Assets/Scripts/LeaderBoardManager.cs
@@ -5,13 +5,12 @@ using GooglePlayGames;
 using UnityEngine.SocialPlatforms;
 using GooglePlayGames.BasicApi;
 
-//using GoogleMobileAds.Api;
-
 
 public class LeaderBoardManager : MonoBehaviour 
 {
 	
 	public static LeaderBoardManager instance;
+	public GameObject LogInButton;
 
 	void Awake()
 	{
@@ -27,8 +26,10 @@ public class LeaderBoardManager : MonoBehaviour
 			PlayGamesPlatform.Instance.Authenticate ((bool success) => {
 				if (success) {
 					/// Signed in! Hooray!
+					LogInButton.SetActive(false);
 				} else {
 					/// Not signed in. We'll want to show a sign in button
+					 LogInButton.SetActive(true);
 				}
 			}, true);   /// <--- That "true" is very important!
 		} else {
@@ -52,7 +53,7 @@ public class LeaderBoardManager : MonoBehaviour
 	/// <summary>
 	/// Login In Into Your Google+ Account
 	/// </summary>
-	public void LogIn ()
+	/*public void LogIn ()
 	{
 		Social.localUser.Authenticate ((success) =>
 			{
@@ -62,14 +63,14 @@ public class LeaderBoardManager : MonoBehaviour
 					Debug.Log ("Login failed");
 				}
 			});
-	}
+	}*/
 	/// <summary>
 	/// Shows All Available Leaderborad
 	/// </summary>
-	/*public void OnShowLeaderBoard ()
+	public void OnShowLeaderBoard ()
 	{
-		//        Social.ShowLeaderboardUI (); // Show all leaderboard
-		((PlayGamesPlatform)Social.Active).ShowLeaderboardUI (//LeaderBoard.leaderboard_highest_score); // Show current (Active) leaderboard
+	     Social.ShowLeaderboardUI (); // Show all leaderboard
+		((PlayGamesPlatform)Social.Active).ShowLeaderboardUI (LeaderBoard.leaderboard_highest_score); // Show current (Active) leaderboard
 	}
 	/// <summary>
 	/// Adds Score To leader board
@@ -77,7 +78,7 @@ public class LeaderBoardManager : MonoBehaviour
 	public void OnAddScoreToLeaderBoard ()
 	{
 		if (Social.localUser.authenticated) {
-			Social.ReportScore (PlayerPrefs.GetInt ("HighScore"), //LeaderBoard.leaderboard_highest_score, (bool success) =>
+			Social.ReportScore (PlayerPrefs.GetInt ("HighScore"), LeaderBoard.leaderboard_highest_score, (bool success) =>
 				{
 					if (success) {
 						Debug.Log ("Update Score Success");
@@ -87,7 +88,7 @@ public class LeaderBoardManager : MonoBehaviour
 					}
 				});
 		}
-	}*/
+	}
 	/// <summary>
 	/// On Logout of your Google+ Account
 	/// </summary>
@@ -96,37 +97,26 @@ public class LeaderBoardManager : MonoBehaviour
 		((PlayGamesPlatform)Social.Active).SignOut ();
 	}
 	#endregion
-	/*void OnStart()
-	{
-		Social.localUser.Authenticate((bool success) => {
-			return;
-		});
-	}
-	// Use this for initialization
-	void Start () 
-	{
-		// PASTE THESE LINES AT THE END OF Start()
-		// Try silent sign-in (second parameter is isSilent)
-		PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
-	}*/
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
 		
 	}
 
-	/*public void Login()
+	public void Login()
 	{
 		 if (!PlayGamesPlatform.Instance.localUser.authenticated) {
             // Sign in with Play Game Services, showing the consent dialog
             // by setting the second parameter to isSilent=false.
             PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+			LogInButton.SetActive(false);
         } else {
             // Sign out of play games
             PlayGamesPlatform.Instance.SignOut();
+			LogInButton.SetActive(true);
 		}
-	}*/
+	}
 
 	public  void AddScoreToLeaderboard()
 	{
@@ -153,45 +143,26 @@ public class LeaderBoardManager : MonoBehaviour
 		}
 		else
 		{
-			LogIn ();
+			Login ();
 		}
 
 	}
 
 	public  void SignInCallback(bool success) {
 		if (success) {
-			Debug.Log("(Lollygagger) Signed in!");
+			Debug.Log("Signed in!");
 
 			// Change sign-in button text
 			//signInButtonText.text = "Sign out";
-
+			LogInButton.SetActive(false);
 			// Show the user's name
 			//authStatus.text = "Signed in as: " + Social.localUser.userName;
 		} else {
-			Debug.Log("(Lollygagger) Sign-in failed...");
-
+			Debug.Log(" Sign-in failed...");
+			LogInButton.SetActive(true);
 			// Show failure message
 			//signInButtonText.text = "Sign in";
 			//authStatus.text = "Sign-in failed";
 		}
 	}
 }
-//PlayGamesPlatform.Activate ();
-
-		/*#if UNITY_ANDROID*/
-		//  ADD THIS CODE BETWEEN THESE COMMENTS
-
-		// Create client configuration
-		//PlayGamesClientConfiguration config = new 
-			//PlayGamesClientConfiguration.Builder()
-			//.Build();
-
-		// Enable debugging output (recommended)
-		//PlayGamesPlatform.DebugLogEnabled = true;
-
-		// Initialize and activate the platform
-		//PlayGamesPlatform.InitializeInstance(config);*/
-		/*PlayGamesPlatform.Activate();
-		// END THE CODE TO PASTE INTO START
-		#endif*/
-		//Login();
